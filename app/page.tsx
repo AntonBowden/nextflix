17448f8ab1a6fff894507f3dcf16420c55e1130c
Anton Bowden <bowden.anton@gmail.com>
2024-04-02 19:58:19 +0100

    create auth page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-// import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Nextflix</h1>
+      <h1 className="text-red-600">Nextflix</h1>
     </main>
   );
 }
